486f709d03fba9f5da6ef241fb136d467a9582e8
stringbitking <victor.tsenkov@gmail.com>
2013-08-14 12:40:34 +0300

    Added repositories.

---
+++ JerryMouseChat/JerryMouse.Repositories/EfRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JerryMouse.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        public EfRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is required to use this repository.", "context");
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        protected IDbSet<T> DbSet { get; set; }
+
+        protected DbContext Context { get; set; }
+
+        public T Add(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Detached)
+            {
+                //this.DbSet.Attach(entity);
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.DbSet.Add(entity);
+            }
+
+            return entity;
+        }
+
+        public T Update(int id, T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet.Attach(entity);
+            }
+
+            entry.State = EntityState.Modified;
+            return entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Deleted)
+            {
+                entry.State = EntityState.Deleted;
+            }
+            else
+            {
+                this.DbSet.Attach(entity);
+                this.DbSet.Remove(entity);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.Get(id);
+
+            if (entity != null)
+            {
+                this.Delete(entity);
+            }
+        }
+
+        public T Get(int id)
+        {
+            return this.DbSet.Find(id);
+        }
+
+        public IQueryable<T> All()
+        {
+            return this.DbSet;
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return this.DbSet.Where(predicate);
+        }
+    }
+}
---
+++ JerryMouseChat/JerryMouse.Repositories/UnitOfWork.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JerryChat.Data;
+using JerryChat.Models;
+
+namespace JerryMouse.Repositories
+{
+    public class UnitOfWork
+    {
+        private JerryChatContext context = new JerryChatContext();
+        private EfRepository<User> usersRepository;
+        private EfRepository<Message> messagesRepository;
+        private EfRepository<Room> roomsRepository;
+
+        public EfRepository<User> UsersRepository
+        {
+            get
+            {
+                if (this.usersRepository == null)
+                {
+                    this.usersRepository = new EfRepository<User>(this.context);
+                }
+
+                return this.usersRepository;
+            }
+        }
+
+        public EfRepository<Message> MessagesRepository
+        {
+            get
+            {
+                if (this.messagesRepository == null)
+                {
+                    this.messagesRepository = new EfRepository<Message>(this.context);
+                }
+
+                return this.messagesRepository;
+            }
+        }
+
+        public EfRepository<Room> RoomsRepository
+        {
+            get
+            {
+                if (this.roomsRepository == null)
+                {
+                    this.roomsRepository = new EfRepository<Room>(this.context);
+                }
+
+                return this.roomsRepository;
+            }
+        }
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
