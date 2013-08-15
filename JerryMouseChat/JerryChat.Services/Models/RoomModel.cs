3eb845f020fee0ab77cfacff8b19626b78803cc3
c0ldwit <rico7@abv.bg>
2013-08-16 00:47:17 +0300

    Last commit

--- JerryMouseChat/JerryChat.Services/Models/RoomModel.cs
+++ JerryMouseChat/JerryChat.Services/Models/RoomModel.cs
@@ -7,10 +7,16 @@ namespace JerryChat.Services.Models
 {
     public class RoomModel
     {
+        public RoomModel()
+        {
+            this.UserIds = new HashSet<int>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int UsersCount { get; set; }
         public bool IsLocked { get; set; }
-        public int AdminId { get; set; } 
+        public int AdminId { get; set; }
+        public ICollection<int> UserIds { get; set; } 
     }
 }
